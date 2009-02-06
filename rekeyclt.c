87273b7874f33fdb61aec510861761436c1694e4
Chaskiel Grundman <cg2v@andrew.cmu.edu>
2009-02-06 15:17:27 +0000

    flesh out admin client some (command line parsing)
    git-svn-id: svn+ssh://vcs-01.andrew.cmu.local/vcs/krb-rekey/projects/krb-rekey/trunk@66 c26f65f0-3115-406c-ae4e-cb6d118b1852

--- rekeyclt.c
+++ rekeyclt.c
@@ -46,6 +46,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <getopt.h>
 #include <openssl/ssl.h>
 #ifdef HEADER_GSSAPI_GSSAPI
 #include <gssapi/gssapi.h>
@@ -56,11 +57,66 @@
 #include "memmgt.h"
 #include "rekey-locl.h"
 #include "rekeyclt-locl.h"
+#include "protocol.h"
 
 int main(int argc, char **argv) {
   SSL *conn;
-
+  char *servername;
+  char *realm=NULL;
+  char *targetname=NULL;
+  char *keytab=NULL;
+  char *cmd;
+  char **hostnames;
+  int flag=0;
+  int optch;
+  
+  while ((optch == getopt(argc, argv, "k:r:d")) != -1) {
+    switch (optch) {
+    case 'k':
+      keytab = optarg;
+      break;
+    case 'r':
+      realm = optarg;
+      break;
+    case 'd':
+      flag|=REQFLAG_DESONLY;
+    case '?':
+      fprintf(stderr, "Usage: rekeyclt [-k keytab] [-r realm] [d] command [args]\n");
+      exit(1);
+    }
+  }
+  cmd = argv[optind++];
+  if ((strcmp(cmd, "start") && strcmp(cmd, "status") && 
+       strcmp(cmd, "abort") && strcmp(cmd, "finalize") && 
+       strcmp(cmd, "key")) || argc - optind < 2 ||
+      (!strcmp(cmd, "start") && (argc - optind < 3))) {
+    fprintf(stderr, "Usage: rekeyclt [-k keytab] [-r realm] [-d] command [args]\n");
+    fprintf(stderr, "       rekeyclt start principalname hostname [hostname]...\n");
+    fprintf(stderr, "       rekeyclt status principalname\n");
+    fprintf(stderr, "       rekeyclt abort principalname\n");
+    fprintf(stderr, "       rekeyclt finalize principalname\n");
+    fprintf(stderr, "       rekeyclt key principalname\n");
+    exit(1);
+  }
+  targertname=argv[optind++];
   ssl_startup();
+  servername = get_server(realm);
+  conn = c_connect(servername);
+  c_auth(conn, servername);
+  if (!strcmp(cmd, "start")) {
+    hostnames = argv + optind;
+    c_newreq(conn, targetname, flag, argc - optind, hostnames);
+  } else if (!strcmp(cmd, "status")) {
+    c_status(conn, targetname);
+  } /*else if (!strcmp(cmd, "abort")) {
+    c_abort(conn, targetname);
+  } else if (!strcmp(cmd, "finalize")) {
+    c_finalize(conn, targetname);
+  } else if (!strcmp(cmd, "key")) {
+    c_simplekey(conn, targetname);
+    } */ else {
+    fprintf(stderr, "??? unimplemented command %s\n", cmd);
+  }
 
   ssl_cleanup();
   return 0;
