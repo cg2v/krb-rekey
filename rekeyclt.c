42f7e5a27b01bc9634693cf65693435e119483fd
Chaskiel Grundman <cg2v@andrew.cmu.edu>
2021-01-07 13:37:25 -0500

    Use correct enctype test
    Update enctype heuristics to use || test instead of &&. This makes
    sure that the compat behavior is triggerred if either affected enctype
    is missing on the client

--- rekeyclt.c
+++ rekeyclt.c
@@ -114,13 +114,13 @@ int main(int argc, char **argv) {
   {
      krb5_context ctx;
      if (krb5_init_context(&ctx) == 0) {
-       if (krb5_enctype_valid(ctx, 17) != ENCTYPE_VALID &&
+       if (krb5_enctype_valid(ctx, 17) != ENCTYPE_VALID ||
            krb5_enctype_valid(ctx, 18) != ENCTYPE_VALID)
          flag|=REQFLAG_COMPAT_ENCTYPE;
        krb5_free_context(ctx);
      }
      if (krb5_init_context(&ctx) == 0) {
-       if (krb5_enctype_valid(ctx, 16) != ENCTYPE_VALID &&
+       if (krb5_enctype_valid(ctx, 16) != ENCTYPE_VALID ||
            krb5_enctype_valid(ctx, 23) != ENCTYPE_VALID)
          flag|=REQFLAG_COMPAT_ENCTYPE_RFC8429;
        krb5_free_context(ctx);
